036f53455a45f925a8999203e949ee8f56cbc1fa
Rohan Menezes <menezesr08@gmail.com>
2022-04-09 16:31:18 +0100

    Added links to register and login views

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/views/login_view.dart';
+import 'package:mynotes/views/register_view.dart';
+import 'package:mynotes/views/verify_email_view.dart';
 
 import 'firebase_options.dart';
 
@@ -15,6 +17,10 @@ void main() {
         primarySwatch: Colors.blue,
       ),
       home: const HomePage(),
+      routes: {
+        '/login/': ((context) => const LoginView()),
+        '/register/': ((context) => const RegisterView()),
+      },
     ),
   );
 }
@@ -22,31 +28,41 @@ void main() {
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
- @override
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomePage'),
-      ),
-      body: FutureBuilder(
+    return FutureBuilder(
         future: Firebase.initializeApp(
           options: DefaultFirebaseOptions.currentPlatform,
         ),
         builder: (context, snapshot) {
           switch (snapshot.connectionState) {
             case ConnectionState.done:
-              final user = FirebaseAuth.instance.currentUser;
-              if(user?.emailVerified ?? false) {
-                print('You are verified');
+            final user = FirebaseAuth.instance.currentUser;
+            if(user != null) {
+              if(user.emailVerified) {
+                print('Email is verified');
               } else {
-                print('You need to verify your email.');
-              }
-              return Text("Done");
+              return const VerifyEmailView();
+              } 
+            } else {
+              return const LoginView();
+            }
+         
+            return const Text('Done');
+              // final user = FirebaseAuth.instance.currentUser;
+              // if (user?.emailVerified ?? false) {
+              // } else {
+              //   return const VerifyEmailView();
+              // }
+              // return const Text("Done");
+              return const LoginView();
+
             default:
-              return const Text("Loading");
+              return const CircularProgressIndicator();
           }
         },
-      ),
-    );
+      );
   }
 }
+
+
