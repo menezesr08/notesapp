53f56257c696cb34aa175f7ef7605db175489959
Rohan Menezes <menezesr08@gmail.com>
2022-05-09 09:24:50 +0100

    refactored routes to bloc

--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -47,8 +47,7 @@ class _NotesViewState extends State<NotesView> {
                  final shouldLogout = await showLogOutDialog(context);
                   if (shouldLogout) {
                     context.read<AuthBloc>().add(const AuthEventLogOut());
-                    Navigator.of(context)
-                        .pushNamedAndRemoveUntil(loginRoute, (route) => false);
+          
                   }
                  
               }
